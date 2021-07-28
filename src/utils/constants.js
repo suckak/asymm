caafcfc5061312ab274dee8d02abb54eee284be3
Daniel Gutiérrez <cdanielgl@gmail.com>
2021-07-28 13:30:36 -0500

    ApiKey updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_KEY = '518d69d4eaf348e1a32273d150ef4268';
+export const API_KEY = 'c1b7eb7930124b8a9cad68a759f47650';
 
 export const API_URL = 'https://api.spoonacular.com/recipes';
